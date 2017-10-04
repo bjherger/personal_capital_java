642b77e142d209f46cf4f46a0b3871691dfd7891
Brendan J. Herger <b@revup.com>
2017-10-04 16:19:04 -0700

    First pass at Portfolio class

--- src/trials/Main.java
+++ src/trials/Main.java
@@ -1,9 +1,23 @@
 package trials;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Test");
+	    // Variables
+        int numTrials = 10000;
+        int numYears = 20;
+        Double startingAmount = 100000.00;
+        Double annualInflation = .035;
+
+
+        // Set up portfolios
+        Portfolio[] portfolioArray = new Portfolio[2];
+        portfolioArray[0] = new Portfolio("agressive", .094324, .15675);
+        portfolioArray[1] = new Portfolio("very_conservative", .06189, .063438);
+        System.out.println("Portfolios: " + Arrays.toString(portfolioArray));
+
+
     }
 }
